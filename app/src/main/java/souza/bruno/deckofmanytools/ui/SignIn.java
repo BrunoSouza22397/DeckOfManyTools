f81ce88846dcad48df8c490b29a5f669e33f1e58
BrunoSouza22397 <45245611+BrunoSouza22397@users.noreply.github.com>
2018-12-20 14:31:17 -0200

    creating google sign in

--- app/src/main/java/souza/bruno/deckofmanytools/ui/SignIn.java
+++ app/src/main/java/souza/bruno/deckofmanytools/ui/SignIn.java
@@ -1,32 +1,104 @@
 package souza.bruno.deckofmanytools.ui;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 
 import com.gigamole.library.PulseView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInApi;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import souza.bruno.deckofmanytools.R;
 
 import static souza.bruno.deckofmanytools.R.id.sign_in_pulse_view;
 
-public class SignIn extends AppCompatActivity {
+public class SignIn extends AppCompatActivity implements View.OnClickListener{
 
     private PulseView pulseView;
+    private Button btSignIn;
+    GoogleApiClient mGoogleApiClient;
+    private static final String TAG = "SignInActivity";
+    private static final int RC_SIGN_IN = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
         pulseView = findViewById(R.id.sign_in_pulse_view);
         pulseView.startPulse();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+
+
+
+        btSignIn = findViewById(R.id.sign_in_button).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         pulseView.finishPulse();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case btSignIn:
+                signIn();
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            //handle signin result
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result){
+        Log.d(TAG, "handleSignInResult: "+result.isSuccess());
+        if (result.isSuccess()) {
+            GoogleSignInAccount acct = result.getSignInAccount();
+        }
+    }
+
+    private void signIn(){
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    private void updateUI(GoogleSignInAccount account){
+//        if (account != null){
+//            startActivity(new Intent(SignIn.this, MainActivity.class));
+//        }
     }
 }
