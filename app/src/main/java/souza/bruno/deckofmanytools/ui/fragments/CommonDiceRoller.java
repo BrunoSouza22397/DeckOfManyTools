c4c9a2c491f77ec81da1b0b41c667025605026cb
Bruno Souza <brunosouza22397@gmail.com>
2018-12-25 20:01:00 -0200

    sign in done

---
+++ app/src/main/java/souza/bruno/deckofmanytools/ui/fragments/CommonDiceRoller.java
@@ -0,0 +1,31 @@
+package souza.bruno.deckofmanytools.ui.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import souza.bruno.deckofmanytools.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CommonDiceRoller extends Fragment {
+
+
+    public CommonDiceRoller() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_common_dice_roller, container, false);
+    }
+
+}
